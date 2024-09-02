b5ff498993969ba187dc52e68329e982776bcf9a
skapri2005 <rntc0822011@nttf.co.in>
2024-09-02 09:42:39 +0530

    Add files via upload

---
+++ Exp14.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Exp14
+{
+public static void main (String []args)
+{
+int a=5;
+int b=0;
+try 
+{
+System.out.println(a/b);
+}
+catch(ArithmeticException e)
+{
+System.out.println(e.getMessage());
+}
+}
+}
